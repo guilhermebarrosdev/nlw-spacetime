885604961acabf046715ff4968a0a5526d2ba8d4
Guilherme Barros <wingui3@gmail.com>
2023-05-16 19:58:20 -0300

    :wrench: Add config eslint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,15 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.post('/hello', () => {
-  return 'Hello World!';
-});
+  return 'Hello World!'
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('🚀 HTTP server running on  http://localhost:3333');
-  });
+    console.log('🚀 HTTP server running on  http://localhost:3333')
+  })
