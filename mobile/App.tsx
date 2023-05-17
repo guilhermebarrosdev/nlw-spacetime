4201bc8b4167688a90a86e8c003f23ac47b1d14e
Guilherme Barros <wingui3@gmail.com>
2023-05-17 00:18:37 -0300

    :tada: Start project mobile react-native with expo

---
+++ mobile/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <Text style={styles.text}>Hello World!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 48
+  }
+});
