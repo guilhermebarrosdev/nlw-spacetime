5e16911c44666b7ac8ec6337cae23c2bb7f3a5fa
Guilherme Barros <wingui3@gmail.com>
2023-05-18 12:05:44 -0300

    :sparkles: Change lang to pt-br

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${roboto.variable} ${baijamjuree.variable} font-sans`}>
         {children}
       </body>
