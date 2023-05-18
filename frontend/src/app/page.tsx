c5a463c0d251eae600c2f25d9564d24d0afb4663
Guilherme Barros <wingui3@gmail.com>
2023-05-18 11:39:30 -0300

    :lipstick: Add roboto and baijamjuree fonts

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-10 text-zinc-50">
       <h1 className="text-4xl font-bold">Sua capsula no tempo</h1>
+      <h1 className="font-alt text-4xl font-bold ">Sua capsula no tempo</h1>
     </div>
   )
 }
