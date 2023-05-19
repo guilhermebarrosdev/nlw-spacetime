27e49a52408995a393bde850da44138658f301da
Guilherme Barros <wingui3@gmail.com>
2023-05-18 23:31:23 -0300

    :wrench: Update tailwindconfig add fontFamily

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        title: 'Roboto_700Bold',
+        body: 'Roboto_400Regular',
+        alt: 'BaiJamjuree_700Bold',
+      },
+      
       colors: {
         gray: {
           50: '#eaeaea',
