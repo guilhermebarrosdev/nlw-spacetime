3b573cbcd5d5db4ea414674ad53c87752d7346f4
Guilherme Barros <wingui3@gmail.com>
2023-05-18 23:18:12 -0300

    :lipstick: Add colors on tailwindcss config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className='flex-1 bg-zinc-950 items-center justify-center'>
-      <Text className='text-zinc-50'>Hello World</Text>
+      <Text className='text-zinc-50'>Hello World !!</Text>
       <StatusBar style="auto" />
     </View>
   );
