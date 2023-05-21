241b5a37c69a533eee9dea12afbfd204c4956185
Guilherme Barros <wingui3@gmail.com>
2023-05-21 00:07:13 -0300

    :fire: Remove import

--- backend/src/routes/memories.ts
+++ backend/src/routes/memories.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
-import { request } from 'http'
 
 export async function memoriesRoutes(app: FastifyInstance) {
   app.get('/memories', async () => {
