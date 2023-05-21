9be4caea43c0fcda5b3e9760adbedce7904d40a2
Guilherme Barros <wingui3@gmail.com>
2023-05-21 04:45:38 -0300

    :lock: Add auth with jwt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,22 @@
 import 'dotenv/config'
 
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // Recomendado por apenas URLS especificas
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
