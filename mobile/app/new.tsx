4998e389d4b374af3674e99fb067aeae09f10614
Guilherme Barros <wingui3@gmail.com>
2023-05-28 02:07:12 -0300

    :sparkles: Create new page with content and styles

---
+++ mobile/app/new.tsx
@@ -0,0 +1,77 @@
+import {
+  Switch,
+  TouchableOpacity,
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+} from 'react-native'
+import { AntDesign, Entypo } from '@expo/vector-icons'
+
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link } from 'expo-router'
+import React, { useState } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+
+  const [isPublic, setIsPublic] = useState(false)
+
+  return (
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+
+        <Link href="/memories" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <AntDesign name="arrowleft" size={16} color="#fff" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+      <View className="m-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            value={isPublic}
+            onValueChange={setIsPublic}
+            trackColor={{ false: '#767577', true: '#372560' }}
+            thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+          />
+          <Text className="font-body text-base text-gray-200">
+            Tornar memoria publica
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
+          <View className="flex-row items-center gap-2">
+            <Entypo name="image" size={12} color="#fff" />
+            <Text className="font-body text-sm text-gray-200">
+              Adicionar foto ou video de capa
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <TextInput
+          multiline
+          className="p-0 font-body text-lg text-gray-50"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+          placeholderTextColor="#56565A"
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="items-center self-end rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">SALVAR</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  )
+}
