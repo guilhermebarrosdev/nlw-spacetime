d958626032d9b38d103e223adebd0fca17161c8f
Guilherme Barros <wingui3@gmail.com>
2023-05-28 02:06:35 -0300

    :sparkles: Add content and styles _layout

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
 import { styled } from 'nativewind'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ImageBackground } from 'react-native'
 import blurBg from '../src/assets/bg-blur.png'
 import Stripes from '../src/assets/stripes.svg'
@@ -17,11 +18,21 @@ import { SplashScreen, Stack } from 'expo-router'
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
 
   if (!hasLoadedFonts) {
     return <SplashScreen />
@@ -41,7 +52,11 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
