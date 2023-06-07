c46cfe257660af1d5f4726022e1fee3191f34b72
Guilherme Barros <wingui3@gmail.com>
2023-06-06 23:59:38 -0300

    :sparkles: Add content memories

--- mobile/app/memories.tsx
+++ mobile/app/memories.tsx
@@ -5,17 +5,41 @@ import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { Link, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import * as SecureStore from 'expo-secure-store'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { api } from '../src/lib/api'
+
+interface Memory {
+  coverUrl: string
+  excerpt: string
+  id: string
+}
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
   const router = useRouter()
+  const [memories, setMemories] = useState<Memory[]>([])
 
   async function singOut() {
     await SecureStore.deleteItemAsync('token')
 
     router.push('/')
   }
+
+  async function loadMemories() {
+    const token = await SecureStore.getItemAsync('token')
+
+    const response = await api.get('/memories', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    setMemories(response.data)
+  }
+
+  useEffect(() => {
+    loadMemories()
+  }, [])
 
   return (
     <ScrollView
@@ -42,17 +66,38 @@ export default function NewMemory() {
       </View>
 
       <View className="mt-6 space-y-10">
-        <View className="space-y-4">
-          <Text>12 de abril, 2023</Text>
-          <View className="space-y-4 px-8">
-            <Image
-              source={{}}
-              className="aspect-video w-full rounded-lg"
-              alt=""
-            />
-            <Text className="font-body text-base leading-relaxed text-gray-100"></Text>
-          </View>
-        </View>
+        {memories.map((memory) => {
+          return (
+            <View className="space-y-4" key={memory.id}>
+              <View className="flex-row items-center gap-2">
+                <View className="h-px w-5 bg-gray-50" />
+                <Text className="font-body text-sm text-gray-100">
+                  12 de abril, 2023
+                </Text>
+              </View>
+              <View className="space-y-4 px-8">
+                <Image
+                  source={{
+                    uri: memory.coverUrl,
+                  }}
+                  className="aspect-video w-full rounded-lg"
+                  alt=""
+                />
+                <Text className="font-body text-base leading-relaxed text-gray-100">
+                  {memory.excerpt}
+                </Text>
+                <Link href="/memories/id" asChild>
+                  <TouchableOpacity className="flex-row items-center gap-2">
+                    <Text className="font-body text-sm text-gray-200">
+                      Ler mais{' '}
+                    </Text>
+                    <AntDesign name="arrowright" size={16} color="#9e9ea0" />
+                  </TouchableOpacity>
+                </Link>
+              </View>
+            </View>
+          )
+        })}
       </View>
     </ScrollView>
   )
